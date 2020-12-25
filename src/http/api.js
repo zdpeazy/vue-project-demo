d73fd66a779aa76b3b7ab407b285ff03fbf7561b
张大朋 <zhangdapeng@edaijia-inc.cn>
2020-12-25 18:41:29 +0800

    first commit

--- src/http/api.js
+++ src/http/api.js
@@ -1,10 +1,8 @@
 import * as login from './moudules/login'
 import * as perms from './moudules/perms'
-import * as user from './moudules/user'
 
 // 默认全部导出
 export default {
   login,
-  user,
-  perms
+  user
 }
